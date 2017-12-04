80897782fd73f52d1aa810adb2f924aecded645f
Leon <jprothwell@gmail.com>
2017-12-04 18:20:54 +0800

    more sb

--- LJAdditions.podspec
+++ LJAdditions.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |s|
 s.name         = "LJAdditions"
-s.version      = "1.0.3"
+s.version      = "1.0.4"
 s.summary      = "LJAdditions"
 s.description  = "awesome! utils"
 s.homepage     = "https://github.com/jprothwell/LJAdditions"
--- LJAdditions/LJAdditions/UIViewController+LJAdditions.m
+++ LJAdditions/LJAdditions/UIViewController+LJAdditions.m
@@ -12,8 +12,31 @@
 
 + (instancetype) lj_instanceViewControllerFromStoryboard{
     NSString* name = NSStringFromClass([self class]);
-    UIStoryboard* storyboard = [UIStoryboard storyboardWithName:name bundle:nil];
+    UIStoryboard* storyboard = nil;
+    @try{
+        storyboard = [UIStoryboard storyboardWithName:name bundle:nil];
+    }
+    @catch(NSException *exception) {
+        NSLog(@"未找到storyboard!");
+        return [[self alloc]init];
+    }
     UIViewController* vc = [storyboard instantiateViewControllerWithIdentifier:name];
+    if (nil == vc) {
+        vc = [storyboard instantiateInitialViewController];
+    }
+    return vc;
+}
+
++ (instancetype) lj_instanceViewControllerFromStoryboardName:(NSString*)name{
+    NSString* className = NSStringFromClass([self class]);
+    UIStoryboard* storyboard = nil;
+    @try {
+        storyboard = [UIStoryboard storyboardWithName:name bundle:nil];
+    }@catch (NSException *exception) {
+        NSLog(@"未找到storyboard!");
+        return [[self alloc]init];
+    }
+    UIViewController* vc = [storyboard instantiateViewControllerWithIdentifier:className];
     if (nil == vc) {
         vc = [storyboard instantiateInitialViewController];
     }
